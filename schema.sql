61dd2fe89c64fe35852ef6da810f7e3495e4e2a3
Veronica Shiyan <GingerRonny@yandex.ru>
2019-04-22 18:43:12 +0300

    Bug_fix

--- schema.sql
+++ schema.sql
@@ -19,16 +19,16 @@ starting_price INT NOT NULL,
 completed_at TIMESTAMP,
 bet_step INT NOT NULL
 );
-CREATE INDEX date_create ON lots(date_create);
+CREATE INDEX created_at ON lots(created_at);
 CREATE INDEX starting_price ON lots(starting_price);
-CREATE INDEX date_completion ON lots(date_completion);
+CREATE INDEX completed_at ON lots(completed_at);
 
 CREATE TABLE bets (
 id INT AUTO_INCREMENT PRIMARY KEY,
 created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
 price INT
 );
-CREATE INDEX date_add ON bets(date_add);
+CREATE INDEX created_at ON bets(created_at);
 CREATE INDEX price ON bets(price);
 
 CREATE TABLE users (
@@ -40,4 +40,4 @@ password CHAR NOT NULL UNIQUE,
 avatar CHAR NOT NULL,
 contact TEXT NOT NULL
 );
-CREATE INDEX date_registration ON users(date_registration);
+CREATE INDEX created_at ON users(created_at);
