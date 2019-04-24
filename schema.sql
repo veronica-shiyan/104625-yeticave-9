29a79f6143ef0e9b1c84ee4d17b9d2014306311d
Veronica Shiyan <GingerRonny@yandex.ru>
2019-04-24 11:09:38 +0300

    Add data in table and write queries

--- schema.sql
+++ schema.sql
@@ -5,19 +5,22 @@ USE yeticave;
 
 CREATE TABLE categories (
 id INT AUTO_INCREMENT PRIMARY KEY,
-name CHAR,
-symbol_code CHAR
+name CHAR (50),
+symbol_code CHAR (50)
 );
 
 CREATE TABLE lots (
 id INT AUTO_INCREMENT PRIMARY KEY,
 created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
-name CHAR NOT NULL,
+name CHAR (255) NOT NULL,
 description TEXT NOT NULL,
-image CHAR NOT NULL,
+image CHAR (50) NOT NULL,
 starting_price INT NOT NULL,
 completed_at TIMESTAMP,
-bet_step INT NOT NULL
+bet_step INT NOT NULL,
+user_id INT NOT NULL,
+winner_id INT,
+category_id INT
 );
 CREATE INDEX created_at ON lots(created_at);
 CREATE INDEX starting_price ON lots(starting_price);
@@ -26,7 +29,9 @@ CREATE INDEX completed_at ON lots(completed_at);
 CREATE TABLE bets (
 id INT AUTO_INCREMENT PRIMARY KEY,
 created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
-price INT
+price INT,
+user_id INT NOT NULL,
+lot_id INT NOT NULL
 );
 CREATE INDEX created_at ON bets(created_at);
 CREATE INDEX price ON bets(price);
@@ -34,10 +39,10 @@ CREATE INDEX price ON bets(price);
 CREATE TABLE users (
 id INT AUTO_INCREMENT PRIMARY KEY,
 created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
-email CHAR NOT NULL UNIQUE,
-login CHAR NOT NULL UNIQUE,
-password CHAR NOT NULL UNIQUE,
-avatar CHAR NOT NULL,
+email CHAR (65) NOT NULL UNIQUE,
+login CHAR (65) NOT NULL UNIQUE,
+password CHAR (255) NOT NULL UNIQUE,
+avatar CHAR (255) NOT NULL,
 contact TEXT NOT NULL
 );
 CREATE INDEX created_at ON users(created_at);
